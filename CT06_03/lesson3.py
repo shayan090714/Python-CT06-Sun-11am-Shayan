cc74435e9e7db1b4da1b597e7e7c92abb7a8d0fb
shayan090714 <kamini_s29@yahoo.com>
2026-01-25 04:26:04 +0000

    End of Lesson 3

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,44 @@
-print("Hello from lesson 3")
+#print("Hello from lesson 3")
+#print("My sentence")
+#age = 50
+#print(age)
+
+#x = 10
+#age = age + x
+#print("when i am 20,i want to get a phone.")
+
+#variable1 = 5
+#variable2 = 3
+#print(variable1 + variable2)
+#print(variable1 - variable2)
+#print(variable1 * variable2)
+#print(variable1 / variable2)
+
+#round(4.567,3)
+#round(4.567,2)
+#round(4.567,1)
+#round(4.567,0)
+#round(4.567,-1)
+#round(4.567-2)
+#round(7.5)
+#round(8.5)
+
+#variable1 = 6
+#variable2 = 5
+#print(variable1 +variable2)
+#print(50-10*3)
+#print(100/4)
+#print((3+4)*5-(10/2))
+
+#variable1 = 2*15
+#variable2 = 1*58
+#print(variable1+variable2)
+
+variable1 = 1*96
+variable2 = 3*18
+variable3 = 6*2
+total=(variable1+variable2+variable3)
+service_charge = 10/100*(total)
+total=total+service_charge
+total = ( total-25)
+print(total)
