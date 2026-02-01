41ac2f3bf2f774a79b5b257b754383b153cefa01
shayan090714 <kamini_s29@yahoo.com>
2026-02-01 04:35:25 +0000

    end of lesson 4

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,42 @@
+from cmath import phase
+
+
 print("Hello from lesson 4")
+
+variable1=100*5
+variable2=20*8
+variable3=2.50*20
+total=(variable1+variable2+variable3)
+service_charge = 10/100*(total)
+total=total+service_charge
+gst=9/100*total
+total=total+gst
+print("$",round(total,1))
+
+#myname=input("shayan")
+#print(myname)
+
+#favourite_colour=input("gold")
+#print(favourite_colour)
+
+#age=input("11")
+#print(age)
+
+#name="Jackie Chan"
+#message="I am hungry"
+#print(name + "says" +message+ " I would like to have instant noodles")
+
+myname="shayan"
+print("Hi"+myname)
+myhobby="football"
+print("I love "+ myhobby)
+dream_vacation="london"
+print("I would love to visit" + dream_vacation)
+
+quantity=3
+amount=17.95
+print(" please pay me$" ,round(quantity*amount,2))
+
+anumber=input("give me a number:")
+anumber=int(anumber*2)
+print("the results is"+str (results) )
