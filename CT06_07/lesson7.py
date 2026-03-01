8cc1d602f05479d47112e6aebb37680617d50087
shayan090714 <kamini_s29@yahoo.com>
2026-03-01 04:31:45 +0000

    End of lesson 7

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,29 @@
 print("Hello from lesson 7")
 
+#score_one = 80
+#score_two = 90
+#score_three = 75
+#total = score_one + score_two + score_three
+#average_score = total / 3
+#student_name = "Alex"
+#print("Average score for " , student_name , " is: " , average_score)
+
+#for count in range(1,11):
+    #print(count)
+
+#for count in range(2,101,2):
+    #print(count)
+
+#for count in range(10,0,-1):
+    #print(count)
+
+#word=input("What would you like to repeat:") 
+#n=input("how many times would you like to repeat:") 
+#n=int(n)
+#for count in range(n):
+    #print("nice to meet you," + word)
+
+anum=int(input("which times table?"))
+for count in range(1,13):
+    answer=
+    print(anum,"x",count,"=",)
