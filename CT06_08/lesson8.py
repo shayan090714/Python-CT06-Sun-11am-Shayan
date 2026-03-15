fdd6005f60256d1215d5528bee6ae24693b39fa9
shayan090714 <kamini_s29@yahoo.com>
2026-03-15 04:31:09 +0000

    End of lesson 8

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,2 +1,44 @@
-print("Hello from lesson 8")
+#total=0
+#for count in range(1,5+1):
+    #answer=input("give me a number #"+ str(count))
+    #total=total+int(answer)
+#print("average=",total/5)
+
+#total=0
+#for count in range(1,5+1):
+    #answer=input("give me a number #"+ str(count))
+    #total=total+int(answer)
+#print("answer=",total*5)
+
+#import time
+#for i in range(10,0,-1):
+    #print(i)
+    #time.sleep(1)
+#print("Liftoff!")
+
+#import random
+#num=random.randint(1,6)
+#print(num)
+
+#import random
+#for count in range(20):
+    #num=random.randint(0,9999)
+    #print(num)
+
+#isholiday=True
+#isfun=True
+#islong=True
+#x="5"
+#y="five"
+#print(not isfun)
+#print (x==y)
+
+import random
+num1=random.randint(1,50)
+num2=random.randint(1,50)
+question="What is"+str(num1)+"+"+str(num2)+"?"
+hiddenanswer=num1+num2
+reply=input(question)
+reply=int(reply)
+print(reply==hiddenanswer)
 
