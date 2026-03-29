6b97155c3df121f6ed497742f0874fb1da2ac310
shayan090714 <kamini_s29@yahoo.com>
2026-03-29 04:27:19 +0000

    End of lesson 10

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,44 @@
-print("Hello from lesson 10")
+#import random
+#num = random.randint(1,15)
+#guess = int(input("What is the number?"))
+#if num == guess:
+#    print("Correct")
+#else:
+#    print("Wrong")
+
+#guess = int(input("Is the number more than zero"))
+#if 0>guess:
+#    print("positive")
+#else:
+#    print("negative")
+
+#age = int(input(" your age ?"))
+#if age<13:
+#    print("child")
+#else:
+#    if age<19:
+#        print("teen")
+#    else:
+#        print("adult")
+
+#temp = int(input("What is the temperature outside now?"))
+#if temp>30:
+#    print("I suggest we go swimming.")
+#elif temp > 26:
+#    print("I suggest we go play basketball.")
+#elif temp > 21:
+#    print("I suggest we go cycling.")
+#else:
+#    print("I suggest we read indoors.")
+
+#score = int(input("What is your score?"))
+#if score>90:
+#    print("You got an 'A'.")
+#elif score > 80:
+#    print("You got a 'B'.")
+#elif score > 70:
+#    print("You got a 'C'.")
+#elif score > 60:
+#    print("You got a 'D'.")
+#else:
+#    print("You got a 'F'")
