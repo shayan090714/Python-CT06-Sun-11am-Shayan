1dad5452aecac03fb5e03f78b42c6f7173248789
shayan090714 <kamini_s29@yahoo.com>
2026-04-05 04:30:21 +0000

    End of lesson 11

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,41 @@
-print("Hello from lesson 11")
+#px = int(input("What is the price of the object"))
+#if px <= 5:
+#    print("Sounds good!")
+#elif px <= 50:
+#    print("Are you sure you need this")
+#elif px <= 500:
+#    print("Where you getting this money from?")
+#else:
+#    print("Don't even think about it!")
+
+#rider1 = 125
+#rider2 = 150
+#if rider1 and rider2>120:
+#    print("You can go for this ride!")
+#else:
+#    print("Sorry. You are not tall enough for this ride.")
+
+#num1 = int(input("Is the number divisible by 3 and 7"))
+#divisibleBy3 = num1 % 3 == 0
+#divisibleBy7 = num1 % 7 == 0
+#   print("Yes it is!")
+#else:
+#    print("No it isn't!")
+
+#rider1 = int(input("What is your age?"))
+#rider2 = int(input("What is your age?"))
+#if rider1 > 18 or rider2 >18:
+#    print("You can go on this Gokart")
+#else:
+#    print("You cannot go on this GoKart")
+
+#age = int(input("What is your age?"))
+#if age < 12 or age > 65:
+#    print("The price of the ticket for you is $15")
+#else:
+#    print("The price of the ticket for you is $20")
+
+colour = input("What is the colour ?")
+isgreen == "green"
+if  not isgreen:
+    print("Try again")
