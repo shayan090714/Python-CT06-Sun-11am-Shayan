6b9683fbca27d2d5a3ec393b73008e4fc48c1567
shayan090714 <kamini_s29@yahoo.com>
2026-04-09 07:41:08 +0000

    2026-04-09

---
+++ project_demo/shopping.py
@@ -0,0 +1,9 @@
+px = int(input("What is the price of the object"))
+if px <= 5:
+   print("Sounds good!")
+elif px <= 50:
+   print("Are you sure you need this")
+elif px <= 500:
+   print("Where you getting this money from?")
+else:
+   print("Don't even think about it!")
