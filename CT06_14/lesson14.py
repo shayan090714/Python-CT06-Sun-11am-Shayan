8af5c6acc86b5ebdaeb83ccfb24652304a992f0f
shayan090714 <kamini_s29@yahoo.com>
2026-04-26 04:29:58 +0000

    End of lesson 14

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,1 +1,31 @@
-print("Hello from lesson 15")
+# counter = 29
+# while counter < 50:
+#      print (counter)
+#      counter = counter + 1
+
+
+# question = "What do you call a bear without a ear"
+# hidden1 = "B"
+# hidden2 = "b"
+# answer = input(question)
+# if answer == hidden1 or answer == hidden2:
+#      print("Correct!")
+# else:
+#      print("Wrong!")
+
+# closet_items = [
+#     "pink dress",
+#     "black skirt",
+#     "black mini dress",
+#     "purple head dress",
+#     "magneta scarf"
+# ]
+# print(closet_items)
+# print("I broke my head band")
+# print("I used the money in my piggy bank to buy a diamond ring")
+# (closet_items[0])= "diamond ring"
+# print(closet_items)
+# print("My mother threw away my black mini dress")
+# del(closet_items[2])
+# print(closet_items)
+
